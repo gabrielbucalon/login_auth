03e5ba83b04eb6f7876d923dc45308ec23d2a5a5
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-10 14:15:26 -0300

    <fix> - upgrade version flutter_svg

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,52 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     //This size provide us total heigth and width of our screen
-    return Backgroung(
-      child: Column(children: <Widget>[
-
-      ],)
-    );
-  }
-  
-}
-
-class Backgroung extends StatelessWidget {
-  final Widget child;
-  const Backgroung({
-    Key key, 
-    @required this.child
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_top.png",
-                width: size.width * 0.3,
-              )),
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_bottom.png",
-                width: size.width * 0.2,
-              )),
-              child,
-        ],
-      ),
-    );
+    return Background(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Welcome To app Flutter Auth",
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        SvgPicture.asset("assets/icons/chat.svg", height: size.height * 0.45),
+        FlatButton(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          color: kPrimaryColor,
+          onPressed: () {},
+          child: Text("LOGIN", style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    ));
   }
 }
