155cc25435c9f98fc2b618b9588583a905fb59f9
gabriel.bucalon <bucalongabriell@gmail.com>
2020-06-10 16:32:39 -0300

    <feature> - create component input_field and input_field_password and rename 'rounded_input_field'

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Login/components/background.dart';
-import 'package:flutter_auth/constants.dart';
+import 'package:flutter_auth/components/rounded_input_field.dart';
+import 'package:flutter_auth/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
@@ -21,54 +22,12 @@ class Body extends StatelessWidget {
           RoundedInputField(
             hintText: "Your Email",
             onChanged: (value) {},
+          ),
+          RoundedPasswordField(
+            onChanged: (value) {},
           )
         ],
       ),
-    );
-  }
-}
-
-class RoundedInputField extends StatelessWidget {
-  final String hintText;
-  final IconData icon;
-  final ValueChanged<String> onChanged;
-  const RoundedInputField({
-    Key key,
-    this.hintText,
-    this.icon: Icons.person,
-    this.onChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFieldContainer(
-      child: TextField(
-        decoration: InputDecoration(
-            icon: Icon(icon, color: kPrimaryColor),
-            hintText: hintText,
-            border: InputBorder.none),
-      ),
-    );
-  }
-}
-
-class TextFieldContainer extends StatelessWidget {
-  final Widget child;
-  const TextFieldContainer({
-    Key key,
-    this.child,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      width: size.width * 0.8,
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(29)),
-      child: child,
     );
   }
 }
